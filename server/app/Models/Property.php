9b711f94a723dcd5d24a50d60934d529d2d0be2a
Souhail <souhailsalimi03@gmail.com>
2025-03-10 12:45:09 +0000

    type

--- server/app/Models/Property.php
+++ server/app/Models/Property.php
@@ -2,9 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Property extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'price',
+        'location',
+        'bedrooms',
+        'bathrooms',
+        'square_feet',
+        'property_type',
+        'is_available',
+    ];
+
+    public $incrementing = false;
+    
+    protected $keyType = 'string';
+
+    protected static function booted()
+    {
+        static::creating(function ($property) {
+            if (empty($property->id)) {
+                $property->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    /**
+     * Define the relationship with the User model.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
