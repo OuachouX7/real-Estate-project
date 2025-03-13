4c0cca3ab1da022e9df7714c0fb1fe7447dccfc4
OuachouX7 <ouachou9@gmail.com>
2025-03-13 18:03:21 +0100

    msg

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState , useEffect } from "react";
 const Navbar = lazy(() => import("../components/navbar"));
 const SearchBar = lazy(() => import("../components/searchBar"));
 
@@ -19,8 +19,8 @@ const Home = () => {
     } catch (error) {
       console.log(error);
     }
-  };
-
+  }
+  
   useEffect(() => {
     getProperties();
   }, []);
@@ -28,25 +28,38 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <SearchBar />
+      <SearchBar/>
+      
 
+  
       <div className="p-10">
-        <div className="mb-10">
-          <div className="flex justify-between items-center">
-            <h2 className="text-3xl font-bold">Listings</h2>
-            <button className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-100">
-              View All
-            </button>
-          </div>
-          <div className="grid grid-cols-4 gap-4 mt-4">
-            {properties.map((listing, index) => (
-              <>// write here</>
-            ))}
-          </div>
+      <div className="mb-10">
+        <div className="flex justify-between items-center">
+          <h2 className="text-3xl font-bold">Listings</h2>
+          <button className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-100">View All</button>
+        </div>
+        <div className="grid grid-cols-4 gap-4 mt-4">
+          {properties.map((listing, index) => (
+            <div key={index} className="p-4 border rounded-lg shadow-sm" onClick={() => navigate(`/property/${listing.id}`)}>
+              <img src={`http://localhost:8000/storage/images/${listing.images[1].image_url}`} alt={listing.name} className="w-full h-48 object-cover rounded-lg" />
+              <h3 className="text-lg font-semibold">{listing.name}</h3>
+              <div className="flex items-center text-gray-600 text-sm">
+                <span className="mr-1">📍</span> {listing.location}
+              </div>
+              <div className="flex justify-between items-center mt-2">
+                <span className="text-xl font-bold">{listing.price}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
     </>
   );
 };
 
 export default Home;
+
+
+
+
