03146a884813881bd39af7e5bd9a54526ae2973a
OuachouX7 <ouachou9@gmail.com>
2025-04-06 23:53:22 +0200

    added button whatsapp link with whatsapp

--- client/src/Components/Map/LocationMap.jsx
+++ client/src/Components/Map/LocationMap.jsx
@@ -25,7 +25,7 @@ const LocationMap = ({ location }) => {
       {position && (
         <MapContainer
           center={position}
-          zoom={13}
+          zoom={4}
           style={{ height: "400px", width: "100%" }}
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
